2fd2456e7a8f474bfabc40f7d26bc8bd67f037d9
stephaniejn <stephaniejnilles@gmail.com>
2015-01-06 10:15:33 -0800

    Updated Formatting

--- bank_transactions.rb
+++ bank_transactions.rb
@@ -1,6 +1,6 @@
 # Challenge 4 - Bank Transactions
 def bank_transaction
-balance = 0
+balance = 0.00
 	answer = "yes"
 	while answer == "yes" do
 	puts "What would you like to do? (deposit, withdraw, check_balance)"
@@ -8,23 +8,23 @@ balance = 0
 	if choice == "deposit"
 		puts "How much would you like to deposit?"
 		number = gets.chomp
-		balance = number.to_i + balance
-		puts "Your current balance is #{balance}"
+		balance = number.to_f + balance
+		puts "Your current balance is % .2f" % balance
 		puts "Would you like to continue?"
 		answer = gets.chomp
 	elsif choice == "withdraw"
 		puts "How much would you like to withdraw?"
-		withdrawl = gets.chomp.to_i
+		withdrawl = gets.chomp.to_f
 		if withdrawl > balance
 			puts "Sorry you don't have an overdraft"
 		else 
 			balance = balance - withdrawl
-			puts "Your new balance is #{balance}"
+			puts "Your new balance is % .2f" % balance
 		end
 		puts "Would you like to continue?"
 		answer = gets.chomp
 	elsif choice == "check_balance"
-		puts 	puts "Your current balance is #{balance}"
+		puts 	puts "Your current balance is % .2f" % balance
 		puts "Would you like to continue?"
 		answer = gets.chomp
 	else 
